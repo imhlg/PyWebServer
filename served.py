31d9744365b60f3b946b7ff827048854e86cc553
imhlg <achalg@outlook.com>
2026-06-12 20:07:36 +0530

    second with folders and bifurcation

--- served.py
+++ served.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
-@app.route('/test')
+@app.route('/world')
 def hello_world():
-    return 'Hello World! from Pycharm on browser'
+    print('hello world')
+    return 'Hello World! from Pycharm on browser and editing more, vene now'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
